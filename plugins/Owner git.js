ea4a3fcf339084ce6492dc469464a93d7ff26964
Chinjumwol <87955538+Ajnasajnas@users.noreply.github.com>
2021-09-16 11:36:46 +0530

    Update Owner git.js

--- plugins/Owner git.js
+++ plugins/Owner git.js
@@ -18,7 +18,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Creater JiHaD*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Creater AJNAS࿐*
 *Best Bgm Bot💘*
 
 *Owner number wa.me/917736936605*
