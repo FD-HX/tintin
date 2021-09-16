d815f4a8c590796d33be8dc15b3f0b2f0be99c1c
Chinjumwol <87955538+Ajnasajnas@users.noreply.github.com>
2021-09-16 09:57:57 +0530

    Update Owner git.js

--- plugins/Owner git.js
+++ plugins/Owner git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://i.imgur.com/PjFla4v.png";
+  r_text[0] ="https://telegra.ph/file/ad4d2ee49c2fc94dfddaa.jpg";
     
     
     var i = Math.floor(1*Math.random())
