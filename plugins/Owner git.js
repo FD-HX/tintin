5418fcdf32d85e8bc6d7151c678039fa5c035566
Chinjumwol <87955538+Ajnasajnas@users.noreply.github.com>
2021-09-16 07:21:14 +0530

    Update Owner git.js

--- plugins/Owner git.js
+++ plugins/Owner git.js
@@ -19,15 +19,15 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Creater JiHaD*
-*Bgm updation💘*
+*Best Bgm Bot💘*
 
-*Owner number wa.me/917736703116*
+*Owner number wa.me/917736936605*
 
-*githublink       https://github.com/J-I-H-A-D/ElsaMwol*
+*githublink       https://github.com/Ajnasajnas/ElsaMwol*
 
-*audio commads    https://github.com/J-I-H-A-D/ElsaMwol/tree/master/uploads*
+*audio commads    https://github.com/Ajnasajnas/ElsaMwol/tree/master/uploads*
 
-*sticker commads  https://github.com/J-I-H-A-D/ElsaMwol/tree/master/stickers*
+*sticker commads  https://github.com/Ajnasajnas/ElsaMwol/tree/master/stickers*
 `}) 
 
 }));
